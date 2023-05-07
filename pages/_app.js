f3afe354ec112d859134d945745b5de7f10ce4b5
Eric Chapman <ericchapman80@gmail.com>
2023-05-07 00:21:50 -0400

    Added modal window and debugging info (#2)
    Co-authored-by: Eric Chapman <ericchapman@MacBook-Pro-3.lan>

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import '../styles/globals.css'
+import "@fullcalendar/common/main.min.css";
+//import "@fullcalendar/daygrid/main.css";
+//import "@fullcalendar/timegrid/main.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useEffect, useState } from 'react';
 import { useRef } from 'react';
-import { Combobox } from '@headlessui/react'
+//import { Combobox } from '@headlessui/react'
 import Image from 'next/image'
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
@@ -9,8 +9,9 @@ import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
-import ReactModal from 'react-modal';
+import Modal from 'react-modal';
 import { Calendar } from '@fullcalendar/core'
+import ReactDOM from 'react-dom';
 
 //import "@fullcalendar/core/main.css";
 //import "@fullcalendar/daygrid/main.css";
@@ -24,10 +25,12 @@ const eventCategoryList = [
   'Volleyball',
 ]
 
+
 export default function Home({ data }) {
   const calendarRef =   useRef(null);
-  //const [screenWidth, setScreenWidth] = useState(null);
-
+  const [screenWidth, setScreenWidth] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [eventInfo, setEventInfo] = useState(null);
 useEffect(() => {
   const calendarApi = calendarRef.current.getApi();
   
@@ -53,6 +56,11 @@ useEffect(() => {
   };
 }, []);
   
+  //Add the handleWindowResize to the eventlistner
+  if (typeof window !== 'undefined') {
+    window.addEventListener('resize', useEffect.handleWindowResize);
+  }
+  
   const [category, setCategory] = useState("All", {
     initialValue: "All",
   });
@@ -62,60 +70,129 @@ useEffect(() => {
     alert(e.target.value);
   };
 
-  //const filteredEvents = events.filter(event => event.category === category);
+  const handleEventClick = (info) => {
+    const event = info.event;
+    // Extract event information
+    var { title, start, end, extendedProps } = event;
+    // Extract extended properties
+    var { category, location, cost, additional_information } = extendedProps;
+    
+    //check for null for non-string items
+    if (start ? start = start.toString() : "");
+    if (end ? end = end.toString() : "");
+    if (cost ? cost = cost.toString() : "");
+    
+
+    setEventInfo({ title, start, end, category, location, cost, additional_information });
+    setIsOpen(true);
+
+    // Render the event information in a modal or populate a div
+    const modal = document.getElementById('eventModal');
+    modal.innerHTML = `
+      <h3>${title}</h3>
+      <p><strong>Start:</strong> ${start} - <strong>End:</strong> ${end} </p>
+      <p><strong>Category:</strong> ${event.extendedProps.category}</p>
+      <p><strong>Location:</strong> ${event.extendedProps.location}</p>
+      <p><strong>Cost:</strong> ${event.extendedProps.cost}</p>
+      <p><strong>Additional Info:</strong> ${event.extendedProps.additional_information}</p>
+    `;
+
+    // Show the modal or update the display of the div
+    modal.style.display = 'block';
+
+    // You can customize the modal closing behavior (e.g., clicking outside the modal)
+    // or close it programmatically when needed
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  // Custom styles for the modal
+  const modalStyle = {
+    overlay: {
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      zIndex: 9999 // Higher z-index value to ensure it appears on to
+    },
+    content: {
+      backgroundColor: '#fff',
+      border: 'none',
+      borderRadius: '4px',
+      boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+      padding: '20px',
+      color: '#333',
+      zIndex: 10000 // Higher z-index value to ensure it appears on top
+    }
+  };
 
   return (
 
     <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}> 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-          <h1>SWVA Event Calendar</h1>
-          {/* <select onChange={handleCategoryChange} value={category}>
-            {eventCategoryList.map((eventCategory) => (
-            <option key={eventCategory} value={eventCategory}>{eventCategory}</option>
-            ))}
-          </select> */}
+          <h1>SWVA Event Calendar</h1>          
+          <select onChange={handleCategoryChange} value={category}>
+        {eventCategoryList.map((eventCategory) => (
+          <option key={eventCategory} value={eventCategory}>{eventCategory}</option>
+        ))}
+      </select>
+      <div id="eventModal" style={{ display: 'none' }}></div>
 
-          <FullCalendar
-            ref={calendarRef} //used to reference calendar element
-            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-            //initialView={screenWidth < 800 ? 'dayGridWeek' : 'dayGridMonth'}
-            initialView={'dayGridMonth'}
-            weekends={true}
-            events={getEventData}
-            //eventClick={handleEventClick}
-            //dateClick={handleDateClick}
-            displayEventTime={false}
-            eventColor={'gray'}
-            //handleWindowResize={handleWindowResize}
-            dayMaxEvents={true}
-            //eventContent={renderEventContent}
-            headerToolbar={{
-              left: 'prev,next today',
-              center: 'title',
-              right: 'dayGridMonth,dayGridWeek,timeGridDay'
-            }}
-            /* eventClick={
-              function(arg){
-                alert(arg.event.title)
-                alert(arg.event.start)
-                alert(arg.event.end)
-              }
-            } */
-          />
+            <FullCalendar
+              editable={true}
+              selectable={true}
+              plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+              initialView='dayGridMonth'
+              weekends={true}
+              events={getEventData}
+              eventClick={handleEventClick}
+              //dateClick={handleDateClick}
+              displayEventTime={false}
+              eventColor={'gray'}
+              //handleWindowResize={handleWindowResize}
+              dayMaxEvents={true}
+              //eventContent={renderEventContent}
+              ref={calendarRef}
+              headerToolbar={{
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth,dayGridWeek,timeGridDay'
+              }}
+              /* eventClick={
+                function(arg){
+                  alert(arg.event.title)
+                  alert(arg.event.start)
+                  alert(arg.event.end)
+                }
+              } */
+            />
+            <Modal
+        isOpen={isOpen}
+        onRequestClose={closeModal}
+        contentLabel="Event Details"
+        style={modalStyle} // Apply custom styles to the modal
+      >
+        <h3>{eventInfo?.title}</h3>
+        <p>
+          <strong>Start:</strong> {eventInfo?.start} - <strong>End:</strong> {eventInfo?.end}
+        </p>
+        <p>
+          <strong>Category:</strong> {eventInfo?.category}
+        </p>
+        <p>
+          <strong>Location:</strong> {eventInfo?.location}
+        </p>
+        <p>
+          <strong>Cost:</strong> {eventInfo?.cost}
+        </p>
+        <p>
+          <strong>Additional Info:</strong> {eventInfo?.additional_information}
+        </p>
+        <button onClick={closeModal}>Close</button>
+      </Modal>
         </div>
     </main>
   );
 }
-
-/* export async function getStaticProps(context) {
-  const sheet = await getDataFromSheets();
-  return {
-    props: {
-      data: sheet.slice(0, sheet.length), // remove sheet header
-    },
-    revalidate: 1, // In seconds
-  };  
-} */
 
 // a custom render function
 function renderEventContent(eventInfo) {
@@ -169,4 +246,4 @@ async function getEventData() {
 
     } catch (error) {
     }
-  }
+  };
