07749f1a13f67d27ce43650df5db68d124f9ed23
Eric Chapman <ericchapman@EricMacookProOG.lan>
2023-05-23 19:06:16 -0400

    Added back calendar.css custom styles and tested performance

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ import "@fullcalendar/common/main.min.css";
 //import "@fullcalendar/daygrid/main.css";
 //import "@fullcalendar/timegrid/main.css";
 
-//import '../styles/calendar.css'; // Import the custom CSS file
+import '../styles/calendar.css'; // Import the custom CSS file
 
 
 export default function App({ Component, pageProps }) {
