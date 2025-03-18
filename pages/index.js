3ae434e3a2a4ddfe1ec045d5ca5a53e48849aab7
Eric Chapman <eric.chapman80@gmail.com>
2025-03-18 00:58:11 -0400

    Timezone take 100

--- pages/index.js
+++ pages/index.js
@@ -328,7 +328,7 @@ export default function Home({ data }) {
           eventColor={'black'}
           dayMaxEvents={true}
           ref={calendarRef}
-          timeZone='local' // Ensure FullCalendar uses UTC
+          timeZone='UTC' // Ensure FullCalendar uses UTC
         />
         <Modal
           isOpen={isOpen}
